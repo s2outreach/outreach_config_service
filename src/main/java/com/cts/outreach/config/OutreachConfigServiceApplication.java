26fc7dabba768d8d28c94c81fd91bc580b086979
Kulanthaivel <539617@cognizant.com>
2019-12-21 09:31:39 +0530

    inital commit

---
+++ src/main/java/com/cts/outreach/config/OutreachConfigServiceApplication.java
@@ -0,0 +1,17 @@
+package com.cts.outreach.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class OutreachConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutreachConfigServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cts/outreach/config/OutreachConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.outreach.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutreachConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
